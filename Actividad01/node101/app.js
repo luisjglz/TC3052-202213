2df91fdf57b6cb322af8392d1bae9503fa261813
Luis José González <68867942+luisjglz@users.noreply.github.com>
2022-08-13 13:12:22 -0500

    Ejercicio en clase: módulos en node.js
    Ejercicio en clase: módulos en node.js

---
+++ Actividad01/node101/app.js
@@ -0,0 +1,11 @@
+const calculadora = require('./mate')
+
+let res = calculadora.suma(1,3)
+
+console.log("********************")
+console.log("*    CALCULADORA   *")
+console.log("*                  *")
+console.log("*  1. Sumar        *")
+console.log("*                  *")
+console.log("********************")
+
---
+++ Actividad01/node101/mate.js
@@ -0,0 +1,7 @@
+exports.suma = function(n1,n2) {
+    return n1+n2
+}
+
+exports.diHola = function(){
+    console.log("Hola")
+}
